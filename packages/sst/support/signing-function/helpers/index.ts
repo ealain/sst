9ec54a04ea9902ef81fe4a411df0adb7b7caca21
Eloi Alain <eloia@theodo.fr>
2023-07-25 15:21:27 +0200

    fix: use original querystring

--- packages/sst/support/signing-function/helpers/index.ts
+++ packages/sst/support/signing-function/helpers/index.ts
@@ -2,5 +2,4 @@ export { getSigV4 } from "./getSigV4";
 export { headerBagToCloudFrontHeaders } from "./headerBagToCloudFrontHeaders";
 export { cloudFrontHeadersToHeaderBag } from "./cloudFrontHeadersToHeaderBag";
 export { queryStringToQueryParameterBag } from "./queryStringToQueryParameterBag";
-export { queryParameterBagToQueryString } from "./queryParameterBagToQueryString";
 export { getRegionFromCustomDomainName } from "./getRegionFromCustomDomainName";
